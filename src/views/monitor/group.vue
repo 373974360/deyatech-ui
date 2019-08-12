3078cf6a1cfb952b8b2c276c55ab240c2a04711c
chaoweima <404827131@qq.com>
2019-08-12 17:13:42 +0800

    隐藏站点选择框

--- src/views/monitor/group.vue
+++ src/views/monitor/group.vue
@@ -265,6 +265,7 @@
             }
         },
         created(){
+            this.$store.state.common.selectSiteDisplay = false;
             this.reloadList();
         },
         methods: {
--- src/views/monitor/site.vue
+++ src/views/monitor/site.vue
@@ -188,6 +188,7 @@
             }
         },
         created(){
+            this.$store.state.common.selectSiteDisplay = false;
             this.reloadList();
         },
         methods: {
