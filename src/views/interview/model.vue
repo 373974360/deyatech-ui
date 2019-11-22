63689d67ed6d495d20b542de2f805259a5e91732
杨崇兴 <yangchongxing@126.com>
2019-11-22 18:22:23 +0800

    富文本显示菜单调整

--- src/views/interview/model.vue
+++ src/views/interview/model.vue
@@ -559,13 +559,97 @@
                     zIndex: 3999,
                     toolbars: [
                         [
-                            'fullscreen', 'source', '|', 'undo', 'redo', '|',
-                            'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'searchreplace','|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
-                            'fontfamily', 'fontsize', '|',
-                            'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|',
-                            'link', 'unlink', '|',
-                            'insertimage', 'attachment','insertvideo','inserttable', '|',
-                            'indent','removeformat', 'formatmatch', 'autotypeset','drafts'
+                            'source', // 源代码
+                            'forecolor', // 字体颜色
+                            'backcolor', // 背景色
+                            '|',
+                            'bold', // 加粗
+                            'italic', // 斜体
+                            'underline', // 下划线
+                            'strikethrough', // 删除线
+                            '|',
+                            'insertorderedlist', // 有序列表
+                            'insertunorderedlist', // 无序列表
+                            '|',
+                            'horizontal', // 分隔线
+                            'superscript', // 上标
+                            'subscript', // 下标
+                            'fontborder', // 字符边框
+                            'spechars', // 特殊字符
+                            'blockquote', // 引用
+                            '|',
+                            'justifyleft', // 居左对齐
+                            'justifycenter', // 居中对齐
+                            'justifyright', // 居右对齐
+                            '|',
+                            'date', // 日期
+                            'time', // 时间
+                            'searchreplace', // 查询替换
+                            'autotypeset', // 自动排版
+                            '|',
+                            //'fullscreen', // 全屏
+                            //'undo', // 撤销
+                            //'redo', // 重做
+                            //'justifyjustify', // 两端对齐
+                            //'selectall', // 全选
+                            //'cleardoc', // 清空文档
+                            //'map', // Baidu地图
+                            //'insertframe', // 插入Iframe
+                            //'background', // 背景
+                            //'insertcode', // 代码语言
+
+                            //'touppercase', // 字母大写
+                            //'tolowercase', // 字母小写
+                        ],
+                        [
+                            'link', // 超链接
+                            'unlink', // 取消链接
+                            'anchor', // 锚点
+                            '|',
+                            'pasteplain', // 纯文本粘贴模式
+                            'removeformat', // 清除格式
+                            'formatmatch', // 格式刷
+                            '|',
+                            'fontfamily', // 字体
+                            'fontsize', // 字号
+                            'paragraph', // 段落格式
+                            'indent', // 首行缩进
+                            'rowspacingtop', // 段前距
+                            'rowspacingbottom', // 段后距
+                            'lineheight', // 行间距
+                            '|'
+                            //'customstyle', // 自定义标题
+                            //'directionalityltr', // 从左向右输入
+                            //'directionalityrtl', // 从右向左输入
+                            //'emotion', // 表情
+                        ],
+                        [
+                            'insertimage', // 多图上传
+                            'imagenone', // 图片默认
+                            'imageleft', // 图片左浮动
+                            'imagecenter', // 图片居中
+                            'imageright', // 右浮动
+                            'attachment', // 附件
+                            'insertvideo', // 视频
+                            '|',
+                            'insertparagraphbeforetable', // 表格前插入行
+                            'inserttable', // 插入表格
+                            'deletetable', // 删除表格
+                            'insertrow', // 前插入行
+                            'insertcol', // 前插入列
+                            'deleterow', // 删除行
+                            'deletecol', // 删除列
+                            'mergecells', // 合并多个单元格
+                            'mergeright', // 右合并单元格
+                            'mergedown', // 下合并单元格
+                            'splittocells', // 完全拆分单元格
+                            'splittorows', // 拆分成行
+                            'splittocols', // 拆分成列
+                            '|'
+                            //'print', // 打印
+                            //'preview', // 预览
+                            //'drafts', // 从草稿箱加载
+                            //'help' // 帮助
                         ]
                     ]
                 },
